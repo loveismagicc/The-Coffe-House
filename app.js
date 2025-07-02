ab3bfa73efdc6406a3ed7117c21b0d5121bcf121
harusora <longcnhe141293@gmail.com>
2025-07-02 19:04:33 +0700

    fix

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+console.log("Mongo URI:", process.env.MONGODB_URI);
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://<username>:<password>@cluster0.mongodb.net/thecoffeehouse', {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
